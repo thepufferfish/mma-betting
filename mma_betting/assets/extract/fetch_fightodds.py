c0936133114595c97ec811e3b02ded4881f51119
thepufferfish <ian.huff2@gmail.com>
2024-08-12 13:56:24 -0700

    feat: fetch odds and odds history for each fight

--- mma_betting/assets/extract/fetch_fightodds.py
+++ mma_betting/assets/extract/fetch_fightodds.py
@@ -14,7 +14,7 @@ def fetch_fights_fightodds(context: AssetExecutionContext, fetch_events_fightodd
     i = 0
     for event in fetch_events_fightodds:
         i += 1
-        context.log.info(f'Fetching fights for event {i} of {len(fetch_events_fightodds)}')
+        context.log.debug(f'Fetching fights for event {i} of {len(fetch_events_fightodds)}')
         fights = api.fetch_event_fights(event['node']['pk'])
         all_fights.append(fights['data'])
     return all_fights
@@ -25,14 +25,27 @@ def fetch_fight_odds(context: AssetExecutionContext, fetch_fights_fightodds):
     i = 0
     for event in fetch_fights_fightodds:
         i += 1
-        context.log.info(f'Fetching odds for event {i} of {len(fetch_fights_fightodds)}')
+        context.log.debug(f'Fetching odds for event {i} of {len(fetch_fights_fightodds)}')
         fights = event['event']['fights']['edges']
         for fight in fights:
             odds = api.fetch_fight_odds(fight['node']['slug'])
             all_odds.append(odds)
-    print(all_odds)
     return all_odds
 
 @asset(key_prefix='fightodds')
-def fetch_odds_history(fetch_fight_odds):
-    pass
+def fetch_odds_history(context: AssetExecutionContext, fetch_fight_odds):
+    all_history = []
+    i = 0
+    for fight in fetch_fight_odds:
+        i += 1
+        context.log.debug(f'Fetching odds history for fight {i} of {len(fetch_fight_odds)}')
+        fight_props = fight['data']['fightPropOfferTable']['propOffers']['edges']
+        for prop in fight_props:
+            offers = prop['node']['offers']['edges']
+            for offer in offers:
+                outcomes = [offer['node'].get('outcome1'), offer['node'].get('outcome2')]
+                for outcome in outcomes:
+                    if outcome:
+                        history = api.fetch_odds_history(outcome['id'])
+                        all_history.append(history)
+    return all_history
