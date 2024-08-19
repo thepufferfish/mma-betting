184390a3cb2256420f48c4995bdd482fecb4b088
thepufferfish <ian.huff2@gmail.com>
2024-08-19 14:04:09 -0700

    fix: drop unneeded functions to fetch scraped tapology data

--- mma_betting/assets/extract/fetch_tapology.py
+++ mma_betting/assets/extract/fetch_tapology.py
@@ -5,19 +5,3 @@ import json
 @op
 def crawl_tapology():
     run_tapology_spider()
-
-@asset(key_prefix='tapology')
-def fetch_events_tapology():
-    events = []
-    with open('tapology_scraper/events.jsonl', 'r') as f:
-        for line in f:
-            events.append(json.loads(line))
-    return events
-
-@asset(key_prefix='tapology')
-def fetch_fights_tapology():
-    fights = []
-    with open('tapology_scraper/fights.jsonl', 'r') as f:
-        for line in f:
-            fights.append(json.loads(line))
-    return fights
