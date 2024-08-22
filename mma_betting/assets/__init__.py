2a11fcff7caacc24d43096c65bb3e5f2560ce1e2
thepufferfish <ian.huff2@gmail.com>
2024-08-22 13:22:57 -0700

    fix: drop json io manager

--- mma_betting/assets/__init__.py
+++ mma_betting/assets/__init__.py
@@ -1,10 +1,6 @@
-from dagster import load_assets_from_package_module, with_resources
-from mma_betting.resources.json_io_manager import JSONIOManager
+from dagster import load_assets_from_package_module
 from . import extract
 
 EXTRACT = 'extract'
 
-extract_assets = with_resources(
-    load_assets_from_package_module(package_module=extract, group_name=EXTRACT),
-    resource_defs={"io_manager": JSONIOManager()}
-)
+extract_assets = load_assets_from_package_module(package_module=extract, group_name=EXTRACT)
