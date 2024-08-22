16fea8fd068e5b2b4b82371d5d46cc48bec7892d
thepufferfish <ian.huff2@gmail.com>
2024-08-22 13:22:45 -0700

    feat: fetch ufcstats events with partitions, create ufcstats fights partitions, and fetch fights with partitions

--- mma_betting/assets/extract/fetch_ufcstats.py
+++ mma_betting/assets/extract/fetch_ufcstats.py
@@ -1,32 +1,33 @@
-from dagster import asset, AssetExecutionContext
-from mma_betting.resources.api_resource import UFCStatsAPIResource
+from dagster import asset, AssetExecutionContext, DynamicOutput
+from mma_betting.resources.api_resources import UFCStatsAPIResource
+from mma_betting.partitions import ufc_events_partitions_def, ufc_fights_partitions_def
 
 api = UFCStatsAPIResource()
 
-@asset(key_prefix='ufc_stats')
-def fetch_ufc_events(context: AssetExecutionContext):
-    all_events = []
-    event_id = 1
-    num_failed = 0
-    while num_failed < 10:
-        context.log.info(f'Fetching event {event_id}')
-        event = api.fetch_event(event_id)
-        if not event.get('LiveEventDetail'):
-            num_failed += 1
-        else:
-            all_events.append(event)
-        event_id += 1
-    return all_events
+@asset(
+    key_prefix='ufc_stats',
+    partitions_def=ufc_events_partitions_def
+)
+def fetch_ufc_event(context: AssetExecutionContext):
+    event_id = context.partition_key
+    context.log.debug(f'Fetching data for event {event_id}')
+    event_data = api.fetch_event(event_id)
+    fights = event_data['LiveEventDetail']['FightCard']
+    for fight in fights:
+        fight_id = fight['FightId']
+        context.log.debug(f'Found fight {fight_id}. Adding to UFC fights partition')
+        context.instance.add_dynamic_partitions(ufc_fights_partitions_def.name, [str(fight_id)])
+    return event_data
 
 
-@asset(key_prefix='ufc_stats')
-def fetch_ufc_fights(context: AssetExecutionContext, fetch_ufc_events):
-    all_fights = []
-    for event in fetch_ufc_events:
-        fights = event['LiveEventDetail']['FightCard']
-        for fight in fights:
-            fight_id = fight['FightId']
-            context.log.info(f'Fetching fight {fight_id}')
-            fight_data = api.fetch_fight(fight_id)
-            all_fights.append(fight_data)
-    return all_fights
+
+@asset(
+    key_prefix='ufc_stats',
+    partitions_def=ufc_fights_partitions_def,
+    deps=[fetch_ufc_event]
+)
+def fetch_ufc_fight(context: AssetExecutionContext):
+    fight_id = context.partition_key
+    context.log.debug(f'Fetching data for fight {fight_id}')
+    fight_data = api.fetch_fight(fight_id)
+    return fight_data
