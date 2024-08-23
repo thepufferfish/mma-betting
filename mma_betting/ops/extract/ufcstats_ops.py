590e1819d8bb6dfe9d17c239132a7321780d1df3
thepufferfish <ian.huff2@gmail.com>
2024-08-23 10:37:02 -0700

    fix: add ufc events partitions as they are discovered and skip existing partitions

--- mma_betting/ops/extract/ufcstats_ops.py
+++ mma_betting/ops/extract/ufcstats_ops.py
@@ -5,19 +5,18 @@ from mma_betting.resources.api_resources import UFCStatsAPIResource
 @op
 def detect_ufcstats_events(context: OpExecutionContext):
     api = UFCStatsAPIResource()
-    event_ids = []
+    existing_partitions = ufc_events_partitions_def.get_partition_keys(dynamic_partitions_store=context.instance)
     event_id = 1
     consecutive_failures = 0
     while consecutive_failures <= 5:
-        context.log.debug(f'Checking UFC API for event {event_id}')
-        event_exists = api.check_event_id(event_id)
-        if event_exists:
-            context.log.debug(f'Found event {event_id}')
-            event_ids.append(str(event_id))
-            consecutive_failures = 0
-        else:
-            context.log.debug(f'Event {event_id} does not exist')
-            consecutive_failures += 1
+        if not str(event_id) in existing_partitions:
+            context.log.debug(f'Checking UFC API for event {event_id}')
+            event_exists = api.check_event_id(event_id)
+            if event_exists:
+                context.log.debug(f'Found event {event_id}, adding to UFC API events partition')
+                context.instance.add_dynamic_partitions(ufc_events_partitions_def.name, [str(event_id)])
+                consecutive_failures = 0
+            else:
+                context.log.debug(f'Event {event_id} does not exist')
+                consecutive_failures += 1
         event_id += 1
-    context.log.info(f'Adding {len(event_ids)} events to UFC API partition')
-    context.instance.add_dynamic_partitions(ufc_events_partitions_def.name, event_ids)
